c2b1bcfe5a6b3f01df0f7d1f71689404e89113eb
Nikit Shuro <i.niksha36@gmail.com>
2024-05-17 15:09:27 +1000

    stop function has been added

--- Bot/bot.py
+++ Bot/bot.py
@@ -10,7 +10,9 @@ bot = telebot.TeleBot(token)
 bot.set_my_commands([
     types.BotCommand(command="/start", description="Запустить бота"),
     types.BotCommand(command="/description", description="Описание бота"),
+    types.BotCommand(command="/stop", description="Отключить бота")
 ])
+
 
 # Define a command handler for the /start command
 @bot.message_handler(commands=['start'])
@@ -20,21 +22,35 @@ def send_welcome(message):
     item2 = types.KeyboardButton("Архитектуры ML")
     markup.add(item1, item2)
 
-    bot.send_message(message.chat.id, "Добропожаловать! Выберите интересующий вас раздел. Чтобы узнать описание введите /description", reply_markup=markup)
+    bot.send_message(message.chat.id,
+                     "Добропожаловать! Выберите интересующий вас раздел. Чтобы узнать описание введите /description",
+                     reply_markup=markup)
+
 
 # Define a command handler for the /description command
 @bot.message_handler(commands=['description'])
 def send_description(message):
-    bot.send_message(message.chat.id, "Этот бот создан для обучения основам машинного обучения. Вы можете выбрать раздел Типы ML или Архитектуры ML для получения информации.")
+    bot.send_message(message.chat.id,
+                     "Этот бот создан для обучения основам машинного обучения. Вы можете выбрать раздел Типы ML или Архитектуры ML для получения информации.")
+
+
+# Define a command handler for the /stop command
+@bot.message_handler(commands=['stop'])
+def stop_bot(message):
+    bot.send_message(message.chat.id, "Бот отключен :(")
+    bot.stop_polling()
+
 
 # Define a message handler
 @bot.message_handler(func=lambda message: message.text == "Типы ML")
 def send_ml_types(message):
     bot.send_message(message.chat.id, "Здесь будут типы ML")
 
+
 @bot.message_handler(func=lambda message: message.text == "Архитектуры ML")
 def send_ml_types(message):
     bot.send_message(message.chat.id, "Здесь будут Архитектуры ML")
+
 
 # Start the bot
 bot.polling()
