3b50edf3e7b524f31b67a4500286e8f2cae7ebc6
Ganzha Eduard <edik.ganzha.05@gmail.com>
2024-05-20 20:31:13 +0900

    Add: README.md, message.json, requirements.txt and Modified: bot.py, .gitignore

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,56 +1,92 @@
+import os
+import json
 import telebot
+import argparse
+
 from telebot import types
 
-with open("Token.txt", "r") as file:
-    token = file.read()
 
-bot = telebot.TeleBot(token)
+def main():
+    # Initialize parser
+    msg = "Start telegram bot: MLWiki"
+    parser = argparse.ArgumentParser(description=msg)
+    parser.add_argument('token', type=str, help="Input token for the telegram bot. It can be obtained at BotFather")
+    args = parser.parse_args()
 
-# Set the list of commands
-bot.set_my_commands([
-    types.BotCommand(command="/start", description="Запустить бота"),
-    types.BotCommand(command="/description", description="Описание бота"),
-    types.BotCommand(command="/stop", description="Отключить бота")
-])
+    # Initialize bot
+    bot = telebot.TeleBot(args.token)
 
-
-# Define a command handler for the /start command
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Типы ML")
-    item2 = types.KeyboardButton("Архитектуры ML")
-    markup.add(item1, item2)
-
-    bot.send_message(message.chat.id,
-                     "Добропожаловать! Выберите интересующий вас раздел. Чтобы узнать описание введите /description",
-                     reply_markup=markup)
+    # Set the list of commands
+    bot.set_my_commands([
+        types.BotCommand(command="/start", description="Запустить бота"),
+        types.BotCommand(command="/description", description="Описание бота"),
+        types.BotCommand(command="/developers", description="Получить информацию о разработчиках"),
+        types.BotCommand(command="/stop", description="Отключить бота"),
+    ])
 
 
-# Define a command handler for the /description command
-@bot.message_handler(commands=['description'])
-def send_description(message):
-    bot.send_message(message.chat.id,
-                     "Этот бот создан для обучения основам машинного обучения. Вы можете выбрать раздел Типы ML или Архитектуры ML для получения информации.")
+    # Define a command handler for the /start command
+    @bot.message_handler(commands=['start'])
+    def send_welcome(message):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Типы ML")
+        item2 = types.KeyboardButton("Архитектуры ML")
+        item3 = types.KeyboardButton("Ресурсы для изучения")
+        markup.add(item1, item2, item3)
+
+        bot.send_message(message.chat.id,
+                         "Добропожаловать! Выберите интересующий вас раздел. Чтобы узнать описание введите /description",
+                         reply_markup=markup)
 
 
-# Define a command handler for the /stop command
-@bot.message_handler(commands=['stop'])
-def stop_bot(message):
-    bot.send_message(message.chat.id, "Бот отключен :(")
-    bot.stop_polling()
+    # Define a command handler for the /description command
+    @bot.message_handler(commands=['description'])
+    def send_description(message):
+        bot.send_message(message.chat.id,
+                         "Этот бот создан для обучения основам машинного обучения.")
 
 
-# Define a message handler
-@bot.message_handler(func=lambda message: message.text == "Типы ML")
-def send_ml_types(message):
-    bot.send_message(message.chat.id, "Здесь будут типы ML")
+    # Define a command handler for the /developers command
+    @bot.message_handler(commands=['developers'])
+    def send_description(message):
+        bot.send_message(message.chat.id,
+                         "Разработчики:\n"
+                         "Ганжа Эдуард Б9123-01.03.04мцм\n"
+                         "Шурло Никита Б9123-01.03.04мцм\n"
+                         "Нестеров Игорь Б9123-01.03.02сп")
 
 
-@bot.message_handler(func=lambda message: message.text == "Архитектуры ML")
-def send_ml_types(message):
-    bot.send_message(message.chat.id, "Здесь будут Архитектуры ML")
+    # Define a command handler for the /stop command
+    @bot.message_handler(commands=['stop'])
+    def stop_bot(message):
+        bot.send_message(message.chat.id, "Бот отключен :(")
+        bot.stop_polling()
+
+
+    # Define a message handler
+    @bot.message_handler(func=lambda message: message.text == "Типы ML")
+    def send_ml_types(message):
+        bot.send_message(message.chat.id, text["types"])
+
+
+    @bot.message_handler(func=lambda message: message.text == "Архитектуры ML")
+    def send_ml_types(message):
+        bot.send_message(message.chat.id, text["architectures"])
+
+    @bot.message_handler(func=lambda message: message.text == "Ресурсы для изучения")
+    def send_ml_types(message):
+        bot.send_message(message.chat.id, text["links"])
+
+    bot.polling(none_stop=True)
 
 
 # Start the bot
-bot.polling()
+if __name__ == "__main__":
+    with open(os.path.dirname(os.path.abspath(__file__)) + "\\message.json", 'r', encoding='utf-8') as f:
+        text = json.load(f)
+
+    while True:
+        try:
+            main()
+        except Exception as e:
+            time.sleep(3)
---
+++ README.md
@@ -0,0 +1,38 @@
+# Telegram Bot - MLWiki
+
+This bot will help you start your journey in learning Machine Learning.
+
+---
+
+## Installation instructions
+1. Cloning the repository
+
+```git clone https://github.com/Niksha36/Telegram_Bot.git```
+
+2. Go to the Telegram_Bot directory
+
+```cd Telegram_Bot```
+
+3. Creating a virtual environment
+
+```python3 -m venv venv```
+
+4. Activation of the virtual environment
+
+```source venv/bin/activate```
+
+5. Installing dependencies
+
+```pip3 install -r requirements.txt```
+
+6. Running a script to demonstrate the capabilities of Bot
+
+```python bot\bot.py --help```
+
+---
+
+## Developers
+
+[Ganzha Eduard](https://github.com/deep-learning-engineer) Б9123-01.03.04мцм\
+[Shurlo Nikita](https://github.com/Niksha36) Б9123-01.03.04мцм\
+[Nesterov Igor](https://github.com/Byter12) Б9123-01.03.02сп
